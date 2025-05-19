2db8046e81178dc9ee2dea3e17aa9717a3cbe3c9
Zaid <ahmedzaid2627@gmail.com>
2025-05-19 19:14:03 +0530

    Nextjs version fix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
-  experimental: {
-     ppr: 'incremental',},
+
   /* config options here */
   env: {
     AUTH_SECRET: process.env.AUTH_SECRET,
